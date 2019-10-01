b75ab330cbbc42465636cd4ae3cd1a1a0d4fb406
Austin Wells <wellsa7@coventry.ac.uk>
2019-10-01 10:19:12 +0100

    Add project files.

---
+++ Game Engine/Game Engine.cpp
@@ -0,0 +1,27 @@
+// Game Engine.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <SDL.h>
+#include <gl\glew.h>
+#include <SDL_opengl.h>
+#include <gl\glu.h>
+#include <stdio.h>
+#include <string>
+#include <enet/enet.h>
+
+int main(int argc, char* args[])
+{
+	
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
